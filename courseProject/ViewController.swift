b20d28fd67dd781c630340ef2dcb73afd6d285fc
lazarevaak (Лазарева Александра) <lazarevaak@yandex.ru>
2025-02-02 23:49:15 +0300

    Initial Commit

---
+++ courseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  courseProject
+//
+//  Created by MacBoock on 02.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
